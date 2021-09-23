2c8e81f1b27e16a37156e150d182e0a1475b39e4
andres <andres.olmos.n@gmail.com>
2021-09-23 15:48:31 -0300

    Webhook test

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import classes from './User.module.scss'
 
 const User = () => {
-  return <div className={classes.User}>User Page</div>
+  return <div className={classes.User}>This is the Profile page</div>
 }
 
 export default User
